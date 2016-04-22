e41cb7574d4f8c8f5b4856c643df9fc961bb53c0
icke <icke@noting.com>
2016-04-22 14:36:53 +0200

    relationship creation is now unique.

--- persistence/graph_management.py
+++ persistence/graph_management.py
@@ -3,7 +3,7 @@ from py2neo import Relationship
 
 
 def create_relationship(source, relation_type, destination, **properties):
-    graph.create(Relationship(source, relation_type, destination, **properties))
+    graph.create_unique(Relationship(source, relation_type, destination, **properties))
 
 
 def find_node(label, name):
